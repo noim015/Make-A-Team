ba0c4242d732819a61073ccf4866cbe3d278dc3c
noim015 <sayed.noim.ahmed.com>
2021-09-26 11:47:39 +0600

    All components creation done

---
+++ src/components/Garment/Garment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Garment = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Garment;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Calculate Total Salary Of Some Employees</h1>
+            <p>Hard work beats talent when talent doesn't work hard.Self-belief and hard work will always earn you success.</p>
+            <h1>Estimate Costing: 10000 BDT</h1>
+        </div>
+    );
+};
+
+export default Header;
